e39ed8f2d1b3b2f68d918f22426c479f87d7344e
HarshKalwani <harshkalwani48@gmail.com>
2024-06-08 17:31:58 +0530

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js"
 import bycryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async (req, res , next ) => {
     const { username, email, password } = req.body;
@@ -20,6 +23,7 @@ export const signup = async (req, res , next ) => {
         })
     }
 
+
     const hashedPassword = bycryptjs.hashSync(password, 10);
 
     const newUser = new User({ username, email, password: hashedPassword });
@@ -34,4 +38,39 @@ export const signup = async (req, res , next ) => {
     }
 
     // console.log(req.body)
+}
+
+export const signin = async(req,res,next) => {
+    const {email , password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({
+            success:false,
+            message:"Fill all the details"
+        })
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        // if(!validUser){
+        //     return res.status(500).json({
+        //         success:false,
+        //         message:"User Not found,"
+        //     })  
+        // }
+
+        //new way 
+        if(!validUser) return next(errorHandler(404, 'User not Found'));
+        const validPassword = bycryptjs.compareSync(password , validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong Credentials!...'));
+        const token = jwt.sign({id:validUser._id} , process.env.JWT_SECRET)
+        validUser.password= undefined
+        res.cookie('access_token',token ,{
+            httpOnly:true,
+        })
+        .status(200)
+        .json(validUser);
+
+    } catch (error) {
+        next(error)
+    }
 }
