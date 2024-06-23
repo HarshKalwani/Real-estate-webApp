8914c56082fb8862b9baeb2cad8fcad52b9332dc
HarshKalwani <harshkalwani48@gmail.com>
2024-06-23 13:07:10 +0530

    Final Edit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800 '>About SuperBalle Estate</h1>
-      <p className='mb-4 text-slate-400'>Sahand Estate is a leading real estate agency that specializes in helping clients buy, sell,
+      <p className='mb-4 text-slate-400'>Superballe Estate is a leading real estate agency that specializes in helping clients buy, sell,
         and rent properties in the most desirable neighborhoods.
         Our team of experienced agents is dedicated to providing exceptional service and making the buying
         and selling process as smooth as possible.</p>
